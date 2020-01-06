852fc9c42207a135e05a6e38e06a84b01b663c90
natalielyn <nlmaxwell@gmail.com>
2020-01-05 18:19:37 -0800

    added code coverage

---
+++ src/filterByTerm.js
@@ -0,0 +1,11 @@
+const filterByTerm = require("../src/filterByTerm");
+
+function filterByTerm(inputArr, searchTerm) {
+    if (!searchTerm) throw Error("searchTerm cannot be empty");
+    if (!inputArr.length) throw Error("inputArr cannot be empty"); // new line
+    const regex = new RegExp(searchTerm, "i");
+    return inputArr.filter(function(arrayElement) {
+      return arrayElement.url.match(regex);
+    });
+  }
+  module.exports = filterByTerm;
